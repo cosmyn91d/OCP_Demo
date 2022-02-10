d9ba223b95ec4e31ceaa3d633b5480252fd5c704
Eduard Cosmin Dita <Eduard-Cosmin.Dita@endava.com>
2022-02-10 08:28:57 +0200

    Master commit 2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,7 +16,8 @@ namespace ConsoleUI
                 new PersonModel { FirstName = "Tim", LastName = "Corey" },
                 new PersonModel { FirstName = "Sue", LastName = "Storm" },
                 new PersonModel { FirstName = "Nancy", LastName = "Roman" },
-                new PersonModel { FirstName = "Albert", LastName = "Mihai" }
+                new PersonModel { FirstName = "Albert", LastName = "Mihai" },
+                new PersonModel { FirstName = "Tom", LastName = "Shelby" }
             };
 
             List<EmployeeModel> employees = new List<EmployeeModel>();
